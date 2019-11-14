b3de764952def77e9c8557ab44b8ad594b50cbe0
Makeda P Mckune <makedamckune@gmail.com>
2019-11-14 19:34:10 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,36 @@ require 'pry'
 class String
 
   def sentence?
+     "Hi, my name is Sophie."
     
+     return true if self.end_with?(".") == true
+     
+     return false if self.end_with?() == false
+     
+          
   end
 
   def question?
-
+      "What's your name?"
+      
+      return true if self.end_with?("?") == true 
+      
+      return false if self.end_with?() == false
   end
 
   def exclamation?
-
+    "Hi, my name is Sophie!"
+    
+    return true if self.end_with?("!") == true 
+    
+    return false if self.end_with?() == false
   end
 
   def count_sentences
-
+    "This, well, is a sentence. This is too!! And so is this, I think? Woo..."
+  
+  self.split(/[.!?]/).map{|x| !(x.match(/\w+/).nil?)}.reject{|x| x == false}.size 
   end
 end
+"This, well, is a sentence. This is too!! And so is this, I think? Woo...".count_sentences
+
